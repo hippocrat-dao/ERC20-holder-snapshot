43315360dbb4e1a850752b17f03d28beb153f959
Hyunhum <hyeonheum@naver.com>
2022-12-07 18:32:02 +0900

    feat: write transferLog.json since ERC20 creation

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const ethers = require('ethers');
+const fs = require('fs');
 const { ERC20_ADDRESS, ERC20_ABI } = require('./constants');
 require('dotenv').config();
 
@@ -8,7 +9,13 @@ const ERC20 = new ethers.Contract(ERC20_ADDRESS, ERC20_ABI, provider);
 
 const filter = new ERC20.filters.Transfer();
 
-const eventLogs = async () => console.log(
-    await ERC20.queryFilter(filter, -1000, "latest"));
+const eventLogs = async () =>
+    await fs.promises.writeFile(
+        'transferLog.json',
+        JSON.stringify(
+            await ERC20.queryFilter(filter,
+                8345679, // from block height(ERC20 created block)
+                "latest" // to block height(snapshot block)
+    )));
 
 eventLogs();
